77bb4b4064fb7eb1aba288500bc7725bf353a23a
Marco <marcosdeseul@gmail.com>
2019-06-27 18:02:00 +0800

    refactor: follow golint

--- main.go
+++ main.go
@@ -13,10 +13,15 @@ func main() {
 	// v2 := "v2"
 	// v3 := "v3"
 	domain := "https://api.brunch.co.kr/"
-	url_profile := fmt.Sprintf("%s/%s/profile/", domain, v1)
-	data := getUserProfile(url_profile, username)
+	urlProfile := fmt.Sprintf("%s/%s/profile/", domain, v1)
+	// url_article := fmt.Sprintf("%s/%s/article/", domain, v2)
+	// https://api.brunch.co.kr/v2/article/@imagineer?lastTime=1525179400000
 
-	prettyPrint(data)
+	profile := getData(urlProfile, username)
+	// article := getData(url_article, username)
+
+	prettyPrint(profile)
+	// prettyPrint(article)
 }
 
 func prettyPrint(data map[string]interface{}) {
@@ -24,11 +29,22 @@ func prettyPrint(data map[string]interface{}) {
 	fmt.Println(string(prettier))
 }
 
-func getUserProfile(url_profile string, username string) map[string]interface{} {
-	url_profile_user := fmt.Sprintf("%s@%s", url_profile, username)
-	resp, _ := http.Get(url_profile_user)
+func getBody(url string) []byte {
+	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
+	return body
+}
+
+func getData(url string, username string) map[string]interface{} {
+	formattedURL := fmt.Sprintf("%s@%s", url, username)
+	body := getBody(formattedURL)
+	return extractData(body)
+}
+
+func getDataWithLastTime(url string, username string, lastTime string) map[string]interface{} {
+	formattedURL := fmt.Sprintf("%s@%s?lastTime=%s", url, username, lastTime)
+	body := getBody(formattedURL)
 	return extractData(body)
 }
 
